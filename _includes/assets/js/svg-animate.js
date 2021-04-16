f215aca099b7ac60231775d36fe9690c2105fdef
David Rhoden <david@davidrhoden.com>
2021-04-16 12:57:43 -0500

    ch ch ch changes....

--- _includes/assets/js/svg-animate.js
+++ _includes/assets/js/svg-animate.js
@@ -1,17 +1,13 @@
 
 document.addEventListener("DOMContentLoaded", function(event) { 
 	const overlay = document.getElementById("product-shape");
-	console.log(overlay);
 	var el = document.getElementsByClassName("color");
 	for (var i = 0; i < el.length; i++) {
-	  console.log(i);
 	  el[i].onclick = changeColor;
 	}
 
 	function changeColor(e) {
-	  console.log(this);
 	  let hex = e.target.getAttribute("data-hex");
-	  console.log(hex);
 	  overlay.style.fill = hex;
 	}
 });
