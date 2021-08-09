6ee1a166fc18b8c62a7c63d00de9b04d82856263
David Rhoden <david@davidrhoden.com>
2021-08-09 17:34:41 -0500

    a list of all tabs, in alphabetical order
    Former-commit-id: 97705d901493261ca0ce76333b8f2f2755c670d4

--- .eleventy.js
+++ .eleventy.js
@@ -72,6 +72,22 @@ module.exports = function(eleventyConfig) {
     return allNotes;
   });
 
+  function filterTagList(tags) {
+    return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+  }
+
+  eleventyConfig.addFilter("filterTagList", filterTagList)
+
+  // Create an array of all tags
+  eleventyConfig.addCollection("tagList", function(collection) {
+    let tagSet = new Set();
+    collection.getAll().forEach(item => {
+      (item.data.tags || []).forEach(tag => tagSet.add(tag));
+    });
+
+    return filterTagList([...tagSet]);
+  });
+
   // Universal slug filter strips unsafe chars from URLs
   eleventyConfig.addFilter("slugify", function(str) {
     return slugify(str, {
