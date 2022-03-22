b97c7bf094c4e63942f14581bef3fe4947dcd381
David Rhoden <david@davidrhoden.com>
2022-03-22 12:21:39 -0500

    try a different build process, remove the minifiers that slow builds down

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const { DateTime } = require("luxon");
-const CleanCSS = require("clean-css");
-const UglifyJS = require("uglify-js");
-const htmlmin = require("html-minifier");
+// const CleanCSS = require("clean-css");
+// const UglifyJS = require("uglify-js");
+// const htmlmin = require("html-minifier");
 const slugify = require("slugify");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const pluginSEO = require("eleventy-plugin-seo");
--- _data/davidrhodenbasicnetlify.js
+++ _data/davidrhodenbasicnetlify.js
@@ -2,4 +2,4 @@ module.exports = {
   environment: process.env.ELEVENTY_ENV
 };
 
- console.log(process.env.ELEVENTY_ENV);
+ // console.log(process.env.ELEVENTY_ENV);
