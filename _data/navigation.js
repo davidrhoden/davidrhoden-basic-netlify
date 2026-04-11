8daf32b4bbafbc0b3cb30d280ca1aed009bacaae
David Rhoden <david@davidrhoden.com>
2026-04-10 21:20:28 -0500

    custom nav data

---
+++ _data/navigation.js
@@ -0,0 +1,88 @@
+module.exports = [
+  {
+    heading: "My work",
+    links: [
+      {
+        title: "Paintings",
+        url: "/paintings/",
+        tooltip: "These are my paintings I took pictures of."
+      },
+      {
+        title: "Illustrations",
+        url: "/illustrations/",
+        tooltip: "Mostly magazine illustrations. Some stuff for corporate presentations."
+      },
+      {
+        title: "Designs",
+        url: "/designs/",
+        tooltip: "Graphic design is my passion."
+      },
+      {
+        title: "Animations",
+        url: "/animations/",
+        tooltip: "In law school a professor asked us if we had dream jobs. I wrote down 'animation'."
+      },
+      {
+        title: "Art For Sale",
+        url: "https://thisismyhappening.com",
+        tooltip: "See my paintings for sale at my other site, thisismyhappening.com.",
+        external: true
+      },
+      {
+        title: "Order a print",
+        url: "/order-form/",
+        tooltip: "Buy my 'I'm Holding In A Lot Of Anger' print. It's $25 with shipping."
+      }
+    ]
+  },
+  {
+    heading: "My history",
+    links: [
+      {
+        title: "Notes/Now",
+        url: "/notes/",
+        tooltip: "Immediate thoughts on the news, technology, or whatever I want to write about."
+      },
+      {
+        title: "Timeline",
+        url: "/timeline/",
+        tooltip: "My whole life story, in blog posts."
+      },
+      {
+        title: "Tags List",
+        url: "/tagslist/",
+        tooltip: "All posts, sorted by every subject I've covered on this website."
+      }
+    ]
+  },
+  {
+    heading: "My CV",
+    links: [
+      {
+        title: "About",
+        url: "/about/",
+        tooltip: "A brief narrative about what I think I was put on the planet for. With pictures."
+      },
+      {
+        title: "Résumé",
+        url: "/resume/",
+        tooltip: "These are the jobs I've had that paid the most."
+      },
+      {
+        title: "Press",
+        url: "/press/",
+        tooltip: "I get interviewed by newspapers and magazines from time to time."
+      }
+    ]
+  },
+  {
+    heading: null,
+    links: [
+      {
+        title: "Get in touch",
+        url: "/contact/",
+        tooltip: "Send me a message, or sign up for my (very fun) online newsletter."
+      }
+    ]
+  }
+];
