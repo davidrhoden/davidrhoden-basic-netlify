8e2690395139ad72787811f7bf2bbc01071d7aef
David Rhoden <david@davidrhoden.com>
2026-04-15 14:11:42 -0500

    add bands collection

--- .eleventy.js
+++ .eleventy.js
@@ -125,6 +125,12 @@ module.exports = function (eleventyConfig) {
       return combined;
     });
 
+  eleventyConfig.addCollection("bands", function (collection) {
+    return collection.getFilteredByGlob("bands/*.md").sort((a, b) => {
+      return a.data.startYear - b.data.startYear;
+    });
+  });
+
   function filterTagList(tags) {
     return (tags || []).filter(
       (tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1,
